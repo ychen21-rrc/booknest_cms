777cff6f248b72f94759b1a187332fdf1fa1dca5
ychen21 <ychen21@academic.rrc.ca>
2025-07-30 15:09:24 -0500

    add favicon

--- header.php
+++ header.php
@@ -15,6 +15,7 @@ $userName = $isLoggedIn ? $_SESSION['user']['username'] : '';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 <body>
     <!-- Navigation -->
